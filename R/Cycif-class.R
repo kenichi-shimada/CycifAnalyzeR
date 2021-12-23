e165bd0597a0a7af0b1476b974dd45bcff41a27d
Kenichi Shimada <kenichi_shimada@hms.harvard.edu>
2021-12-23 15:27:01 -0500

    defined new classes: Cycif, CycifStack, CellType*

---
+++ R/Cycif-class.R
@@ -0,0 +1,95 @@
+#' @export
+setClass("Cycif",
+         slots = c(
+           ## using qts
+           name = "character",
+           suffix = "character",
+
+           abs_list = "data.frame",
+           used_abs = "vector",
+
+           raw = "data.frame",
+           normalized = "data.frame",
+           normalize.method = "character",
+           dna = "data.frame",
+
+           n_cycles = "numeric",
+           n_cells = "numeric",
+
+           xy_coords = "data.frame",
+           segment_property = "data.frame",
+
+           cell_type = "CellTypeCycif",
+           threshold = "numeric", ## this is going to be discontinued
+
+           used_cells = "matrix", # matrix
+           dna_thres = "data.frame",
+           area_filter= "vector",
+
+           ld_coords = "data.frame",
+           clusters = "numeric", # numeric
+
+           tif_files = "data.frame", # full path
+           dim = "data.frame", # size of the tif_file
+           rect = "list",
+
+           segmentation_masks = "character",
+
+           commands = "list"
+         )
+)
+
+#' @export
+Cycif <- function(filename,suffix="_cellMask") {
+  stopifnot(file.exists(filename))
+
+  name <- sub("unmicst-(.+)\\.csv","\\1",filename)
+
+  txt <- read.csv(filename)
+  n.cells <- nrow(txt)
+
+  cn <- names(txt)
+  is.ch <- grepl(suffix,cn)
+  ch.list <- sub(suffix,"",cn[is.ch])
+  i.dna <- grep("^DNA",ch.list)
+  ab.list <- ch.list[-i.dna]
+
+  n.abs <- length(ab.list)
+  n.cycles <- (n.abs/3)
+  abs_list <- data.frame(ab=ab.list,
+                         cycle=rep(seq(n.cycles)-1,each=3),
+                         channel=rep(1:3,times=n.cycles),
+                         stringsAsFactors=TRUE)
+
+  abs_params <- data.frame(ab=ab.list,
+                           low=rep(-Inf,n.abs),
+                           high=rep(Inf,n.abs),
+                           use=rep(TRUE,n.abs))
+
+  dna.list <- ch.list[i.dna]
+
+  ## raw
+  sub.txt <- txt[is.ch]
+  names(sub.txt) <- ch.list
+
+  raw <- sub.txt[ab.list]
+  dna <- sub.txt[dna.list]
+
+  xy_coords <- txt[c("X_centroid","Y_centroid")]
+  # xy_coords$Y_centroid <- max(xy_coords$Y_centroid) - xy_coords$Y_centroid
+
+  segment_property <- txt[c("Area","MajorAxisLength",
+                            "MinorAxisLength", "Eccentricity","Solidity",
+                            "Extent","Orientation")]
+
+  new("Cycif",
+      name = name,
+      suffix = suffix,
+      abs_list = abs_list,
+      raw = raw,
+      dna = dna,
+      n_cycles = n.cycles,
+      n_cells = n.cells,
+      xy_coords = xy_coords,
+      segment_property = segment_property)
+}
---
+++ R/CycifStack-class.R
@@ -0,0 +1,61 @@
+#' @export
+setClass("CycifStack",
+         slots = c(
+           names = "character",
+           suffix = "character",
+
+           uniq_abs = "data.frame",
+
+           n_samples = "numeric",
+           n_cycles = "numeric",
+           max_cycles = "numeric",
+           n_cells = "numeric",
+
+           cell_type = "CellTypeCycifStack",
+           threshold = "data.frame",
+
+           raw = "data.frame",
+           normalized = "data.frame",
+           normalize.method = "character",
+           ld_coords = "data.frame",
+           samples = "list"
+         )
+)
+
+#' @export
+CycifStack <- function(filenames){
+  stopifnot(all(file.exists(filenames)))
+  cat("Trying to load ",length(filenames)," samples.\n",sep="")
+
+  n.samples <- length(filenames)
+  samples <- lapply(filenames,function(filename){
+    name <- sub("unmicst-(.+)\\.csv","\\1",filename)
+    cat("Loading ",name,"...\n",sep="")
+    smpl <- Cycif(filename)
+    return(smpl)
+  })
+
+  suffix <- samples[[1]]@suffix
+  nms <- 	names(samples) <- sapply(samples,names)
+  n_cells <- 	sapply(samples,nCells)
+  n_cycles <- sapply(samples,nCycles)
+  max_cycles <- max(n_cycles)
+
+  if(0){
+    ###### "Validate if samples with max_cycles use uniq_abs."
+  }else{
+    idx <- which(n_cycles == max_cycles)[1]
+  }
+
+  uniq_abs <- abs_list(samples[[idx]])
+  new("CycifStack",
+      names = nms,
+      suffix = suffix,
+      uniq_abs = uniq_abs,
+      n_samples = n.samples,
+      n_cycles = n_cycles,
+      max_cycles = max_cycles,
+      n_cells = n_cells,
+      samples = samples
+  )
+}
