aab8bef6fec8e3c3061558396de5eb1a393f2823
Kenichi Shimada <kenichi_shimada@hms.harvard.edu>
2022-12-13 09:52:04 -0500

    change in show cycif object

--- R/cycif.R
+++ R/cycif.R
@@ -84,12 +84,13 @@ setMethod("show", "Cycif", function(object) {
   rownames(m) <- c("R","G","B")
   colnames(m) <- paste0("Cycle",seq(nCycles(object)))
   m <- data.frame(m)
-  nd <- nrow(dna_thres(x)) > 0
-  nln <- nrow(x@log_normalized) > 0
-  nltn <- nrow(x@logTh_normalized) > 0
-  nct <- length(x@cell_type@cell_types) > 0
+
+  nd <- nrow(dna_thres(object)) > 0
+  nln <- nrow(object@log_normalized) > 0
+  nltn <- nrow(object@logTh_normalized) > 0
+  nct <- length(object@cell_type@cell_types) > 0
   # cat(m)
-  cat(is(object)[[1]], "\n",
+  cat("[",is(object)[[1]], " object]\n\n",
       "Name: ", object@name, "\n",
       "nCells:  ", object@n_cells, "\n",
       "nCycles: ", object@n_cycles, "\n\n",
