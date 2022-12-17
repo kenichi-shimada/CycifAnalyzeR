429b53cd597447a8921af8e1e34ce15dbd30b30d
Kenichi Shimada <kenichi_shimada@hms.harvard.edu>
2022-12-17 16:10:10 -0500

    Added transpose=T for plotting long cell type names

--- R/CellTypeGraph.R
+++ R/CellTypeGraph.R
@@ -1,11 +1,18 @@
-CellTypeGraph <- function(ctype,plot=F){
+CellTypeGraph <- function(ctype,plot=F,transpose=T){
+  uniq.cts <- c("all",ctype$Child)
   ctgraph <- ctype[c("Parent","Child")]
+  ctgraph$Parent <- factor(ctgraph$Parent,levels=uniq.cts)
+  ctgraph$Child <- factor(ctgraph$Child,levels=uniq.cts)
   g <- igraph::graph_from_data_frame(ctgraph)
   l <- igraph::layout_as_tree(g)
   levs <- l[,2]
   names(levs) <- names(igraph::V(g))
   ctlevs <- rev(tapply(names(levs),levs,identity))
   if(plot){
+    if(transpose){
+      l[,2] <- max(l[,2]) - l[,2]
+      l <- l[,2:1]
+    }
     plot(g,layout=l)
   }
   return(ctlevs)
