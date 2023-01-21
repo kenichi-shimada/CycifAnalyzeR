312cb748c320c12f6cdaf72bfb0b745fac18703c
Kenichi Shimada <kenichi_shimada@hms.harvard.edu>
2023-01-21 18:41:53 -0500

    heatmapSummAb (plot_type=="freq") is adjusted so color doens't have to be exactly between [0,1]

--- R/heatmapSummAb.R
+++ R/heatmapSummAb.R
@@ -23,14 +23,6 @@ setMethod("heatmapSummAb", "CycifStack",
       stop("ab should be always specified")
     }
 
-    if(norm_type=="log_normalized"){
-      balanceColor <- FALSE
-    }else if(norm_type=="logTh_normalized"){
-      balanceColor <- TRUE
-      scale <- "none"
-    }
-    ## cell types
-    cts <- cell_types(x,ctype.full=ctype.full,leaves.only=TRUE,within.rois=rois,strict=strict)
 
     if(grepl("percentile$",sum_type)){
       pct <- strsplit(sum_type," ")[[1]]
@@ -42,20 +34,37 @@ setMethod("heatmapSummAb", "CycifStack",
       sum_fun=function(y,...)mean(y,na.rm=T)
     }else if(sum_type=="freq"){
       thres <- unlist(x@cell_type@gates[ab,]) # identical whichever ctype.full
-      sum_fun=function(y,th)sum(y > th,na.rm=T)/sum(!is.na(y))
+      norm_type="logTh_normalized"
+      sum_fun=function(y,th){
+        if(length(y)==0){
+          return(NA)
+        }else{
+          return(sum(y > 0.5,na.rm=T)/sum(!is.na(y)))
+        }
+      }
     }
+
+    if(sum_type=="freq" || norm_type=="log_normalized"){
+      balanceColor <- FALSE
+    }else if(norm_type=="logTh_normalized"){
+      balanceColor <- TRUE
+      scale <- "none"
+    }
+
+    ## cell types
+    cts <- cell_types(x,ctype.full=ctype.full,leaves.only=TRUE,within.rois=rois,strict=strict)
 
     df <- exprs(x,type=norm_type) %>%
       tibble::rownames_to_column(var="smpl") %>%
       mutate(smpl = sub("\\.[0-9]+$","",smpl)) %>%
       mutate(celltype=factor(cts))
 
-
     if(missing(uniq.smpls)){
       uniq.smpls <- unique(df$smpl)
     }
     if(missing(uniq.cts)){
       uniq.cts <- levels(df$celltype)
+      uniq.cts <- uniq.cts[!uniq.cts %in% c("Immune_other","unknown")]
     }
 
     pd <- pData(x) %>% rename(smpl=id)
@@ -64,6 +73,7 @@ setMethod("heatmapSummAb", "CycifStack",
       filter(!is.na(celltype)) %>%
       filter(smpl %in% uniq.smpls) %>%
       filter(celltype %in% uniq.cts) %>%
+      mutate(celltype=factor(celltype,levels=uniq.cts)) %>%
       rename(this_ab=as.symbol(ab)) %>%
       mutate(thres1 = thres[smpl]) %>%
       group_by(smpl,celltype) %>%
@@ -73,12 +83,15 @@ setMethod("heatmapSummAb", "CycifStack",
 
     ttl <- paste0(ab,",",sum_type,",",sub("_.+","",norm_type),",(scale:",scale,")")
     m1 <- as.matrix(with(df, sparseMatrix(as.integer(smpl), as.integer(celltype), x=sum_ab)))
-    m1[m1==0] <- NA
+    m1[apply(m1,1,function(x)any(is.nan(x))),] <- NA
+    if(sum_type!="freq"){
+      m1[m1==0] <- NA
+    }
     rownames(m1) <- levels(df$smpl)
     colnames(m1) <- levels(df$celltype)
     m1 <- m1[rev(seq(nrow(m1))),]
 
-    if(norm_type=="logTh_normalized"){
+    if(sum_type!="freq" && norm_type=="logTh_normalized"){
       m1 <- m1 - 0.5 ## hardcode!!!!
     }
 
@@ -88,7 +101,6 @@ setMethod("heatmapSummAb", "CycifStack",
        cexRow=.7,
        na.rm = T,
        balanceColor=balanceColor,
-       scale=scale,
-       ...)
+       scale=scale,...)
 })
 
