85b811a1b80fe9954e033fc1d1340e6444e5f204
Kenichi Shimada <kenichi_shimada@hms.harvard.edu>
2023-01-31 14:41:07 -0500

    nCycles() now checks and updates all relevant channels upon changes in nCycles.

--- R/nCycles.R
+++ R/nCycles.R
@@ -19,6 +19,7 @@ setMethod("nCycles", "CycifStack", function(x){
 #' @export
 setGeneric("nCycles<-", function(x,...) standardGeneric("nCycles<-"))
 setMethod("nCycles<-", "Cycif", function(x,value){
+
   require(dplyr)
   if(!is.numeric(value)){
     stop("value should be numeric")
@@ -29,6 +30,10 @@ setMethod("nCycles<-", "Cycif", function(x,value){
   if(any(value > current.nc)){
     stop("New max n_cycles should be less than or equal to the current max n_cycles.")
   }
+
+  ## n_cycles, abs_list, raw, log_normalized, logTh_normalized, dna,
+  ## dna_thres
+  x@n_cycles <- value
 
   x@abs_list <- abs_list(x) %>% filter(cycle <= value)
   this.abs <- as.character(abs_list(x)$ab)
@@ -43,48 +48,43 @@ setMethod("nCycles<-", "Cycif", function(x,value){
 
   x@dna <- x@dna[paste0("DNA",seq(value))]
 
-  x@n_cycles <- value
-
   if(nrow(x@used_cells)>0){
     x@used_cells <- x@used_cells[,seq(value)]
   }
 
-  x@n_cycles <- value
+  if(nrow(x@dna_thres)>0){
+    x@dna_thres <- x@dna_thres[paste0("DNA",seq(value)),]
+  }
+
+  if(length(x@rois)>0){
+    ncys <- sapply(x@rois,function(y)y$cycle)
+    x@rois <- x@rois[ncys <= value]
+  }
   return(x)
 })
 
 setMethod("nCycles<-", "CycifStack", function(x,value){
-  if(!is.numeric(value)){
-    stop("value should be numeric")
+  if(!is.numeric(value) | length(value) != 1){
+    stop("CycifStack@nCycles should be a single numeric value now (not accepting a vector with length of x)")
   }
-  smpls <- names(x)
-  names(smpls) <- NULL
-  if(!identical(smpls,names(value))){
-    stop("Input should be a vector whose names are identical to sample names. If fewer samples are selected for analysis, first subset samples with [] then use this func.")
-  }
+
+  ## subsetting samples - trimming ones not reaching the cycle specified
+  ncys <- cyApply(x,nCycles,simplify=T)
+  is.used.smpls <- ncys >= value
+  x <- x[is.used.smpls]
 
   x <- cyApply(x,function(this){
     # return(class(this))
     smpl <- names(this)
-    nCycles(this) <- value[smpl]
+    nCycles(this) <- value
     return(this)
   })
 
   x@n_cycles <- value
-  x@max_cycles <- max(value)
+  x@max_cycles <- value # will be discontinued
 
-  if(0){
-    ###### "Validate if samples with max_cycles use abs_list."
-  }else{
-    idx <- which(x@n_cycles == x@max_cycles)[1]
-  }
-
-  x@abs_list <- abs_list(x[[idx]])
-  this.abs <- as.character(abs_list(cs1)$ab)
-
-  # if(nrow(x@raw)>0){
-  #   x@raw <- x@raw[this.abs]
-  # }
+  x@abs_list <- abs_list(x[[1]]) ## all the samples should have same # of cycles
+  this.abs <- as.character(abs_list(x)$ab)
 
   if(all(this.abs %in% names(x@log_normalized))){
     x@log_normalized <- x@log_normalized[this.abs]
@@ -92,6 +92,16 @@ setMethod("nCycles<-", "CycifStack", function(x,value){
 
   if(all(this.abs %in% names(x@logTh_normalized))){
     x@logTh_normalized <- x@logTh_normalized[this.abs]
+  }
+
+  x@names <- smpls <- names(x)
+  x@n_samples <- length(x)
+  x@samples <- x@samples[smpls]
+
+  if(nrow(x@phenoData)>0){
+    x@phenoData <- x@phenoData %>%
+      filter(id %in% smpls) %>%
+      arrange(match(smpls, id))
   }
 
   return(x)
