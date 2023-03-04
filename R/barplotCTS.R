55f93ca2577ac89bcc6cb9fa5121e25071a718ef
Kenichi Shimada <kenichi_shimada@hms.harvard.edu>
2023-03-04 18:07:28 -0500

    update barplotCTS (note that there's another function, barplot_cts; don't be confused between the two)

--- R/barplotCTS.R
+++ R/barplotCTS.R
@@ -1,8 +1,64 @@
 #' @export
-barplotCTS <- function(n.sh,uniq.cols,ylim=c(0,1)){
-  nf <- layout(as.matrix(c(6:1)),widths=1,heights=c(1,1,1,1,1,16))
+barplotCTS <- function(n.sh,anno.smpls,uniq.cols,ylim=c(0,1)){
+  nf <- layout(as.matrix(c(6:1,7)),widths=1,heights=c(1,1,1,1,1,1,16))
+  if(!all(colnames(n.sh)==anno.smpls$id)){
+    stop("colnames(n.sh) and anno.smpls$id should be the sample names with identical order.")
+  }
+
   ##
+  xs <- seq(ncol(n.sh))
+  range.xs <- range(xs) + c(-1,1)*1.75
+
+  ## 1: plot BOR
+  par(mar=c(0,7,.5,10))
+  par(tcl=0)
+  image(xs,1,as.matrix(as.numeric(anno.smpls$BOR)),col=brewer.pal(3,"Set1")[c(1,3,2)],axes=F,
+       xlab="",ylab="",xlim=range.xs)
+  axis(2,at=1,labels=c("BOR"),las=1)
+
+  ## 2: plot PFS
+  par(mar=c(0,7,.5,10))
+  pfs <- anno.smpls$PFS_days
+  pfs.idx <- pfs - min(pfs,na.rm=T) + 1
+  pfs.uniq.cols <- colorRampPalette(brewer.pal(9,"Blues"))(max(pfs.idx,na.rm=T))
+  pfs.col <- c("grey80",pfs.uniq.cols[unique(sort(pfs.idx))])
+  pfs.idx[is.na(pfs.idx)] <- -1
+  pfs.idx <- as.numeric(factor(pfs.idx))
+
+  par(tcl=0)
+  image(xs,1,as.matrix(pfs.idx),col=pfs.col,axes=F,
+        xlab="",ylab="",xlim=range.xs)
+  axis(2,at=1,labels=c("PFS"),las=1)
+
+  ## 3: plot Time point
+  par(mar=c(0,7,.5,10))
+  image(xs,1,as.matrix(as.numeric(anno.smpls$TimePoint)),col=brewer.pal(4,"Set2"),axes=F,
+       xlab="",ylab="",xlim=range.xs)
+  axis(2,at=1,labels=c("Time point"),las=1)
+
+  ## 4: plot Subtype
+  par(mar=c(0,7,.5,10))
+  image(xs,1,as.matrix(as.numeric(anno.smpls$Subtype)),col=brewer.pal(3,"Dark2")[1:2],axes=F,
+       xlab="",ylab="",xlim=range.xs)
+  axis(2,at=1,labels=c("Subtype"),las=1)
+
+  ## 5: plot gBRCA
+  par(mar=c(0,7,.5,10))
+  image(xs,1,as.matrix(as.numeric(factor(anno.smpls$gBRCA.status))),col=c("grey20","grey80"),axes=F,
+       xlab="",ylab="",xlim=range.xs)
+  axis(2,at=1,labels=c("gBRCA.status"),las=1)
+
+  ## 6: plot Patient
+  par(mar=c(0,7,.5,10))
+  par(tcl=0)
+  image(xs,1,as.matrix(as.numeric(anno.smpls$Patient)),col=c(brewer.pal(12,"Set3"),brewer.pal(8,"Pastel1")),
+        axes=F,
+       xlab="",ylab="",xlim=range.xs)
+  axis(2,at=1,labels=c("Patient"),las=1)
+
+  ## 7: main barplot
   par(mar=c(7,7,1,10))
+  par(tcl=-0.5)
 
   ymax <- ylim[2]
   if(ymax!=1){
@@ -28,10 +84,10 @@ barplotCTS <- function(n.sh,uniq.cols,ylim=c(0,1)){
 
   par(xpd=T)
   legend(par()$usr[2],par()$usr[4],levels(anno.smpls$BOR),fill=brewer.pal(3,"Set1")[c(1,3,2)],title="BOR")
-  legend(par()$usr[2],.8*ymax,levels(anno.smpls$TimePoint),fill=brewer.pal(3,"Set2"),title="TimePoint")
-  legend(par()$usr[2],.55*ymax,c("MUT","WT"),fill=c("grey80","black"),title="gBRCA")
+  legend(par()$usr[2],.8*ymax,levels(anno.smpls$TimePoint),fill=brewer.pal(4,"Set2"),title="TimePoint")
+  legend(par()$usr[2],.55*ymax,c("MUT","WT"),fill=c("grey20","grey80"),title="gBRCA")
   legend(par()$usr[2],.375*ymax,levels(anno.smpls$Subtype),fill=brewer.pal(3,"Dark2"),title="Subtype")
-  legend(par()$usr[2],0.2*ymax,sub("CD68_CD163_","DP_",rev(rownames(n.sh))[-1]),fill=rev(uniq.cols)[-1],title="cell types")
+  legend(par()$usr[2],0.2*ymax,sub("CD68_CD163_","DP_",rev(rownames(n.sh))),fill=rev(uniq.cols),title="Cell types")
   par(xpd=F)
   par(fg=NA)
   axis(1,at=xidx,labels=names(med.x),las=2,cex.axis=.8)
@@ -39,42 +95,11 @@ barplotCTS <- function(n.sh,uniq.cols,ylim=c(0,1)){
 
   d <- diff(xx[1:2])
   par(xpd=T)
-  pts <- (pData(cs1) %>% filter(id %in% names(cs1)))$Patient.ID
-  vs <- c(0,xx[cumsum(table(pts))] + d/2)
+  pts <- factor(sub.pd$Patient.ID,levels=unique(sub.pd$Patient.ID))
+  vs <- c(xx[1]-d/2,xx[cumsum(table(pts))] + d/2)
   sapply(vs,function(x1){
-   lines(rep(x1,2),c(-0.05,1.05),lty=2)
+    lines(rep(x1,2),c(-0.05,1.4),lty=2)
   })
-  text(xx[cumsum(table(pts))],1.15,unique(pts),srt=45,cex=.5)
-  # legend(par()$usr[2],par()$usr[4],rev(names(uniq.cols)),fill=rev(uniq.cols),cex=.7,border=NA,box.col=NA)
   par(xpd=F)
 
-  ##
-  par(mar=c(0,7,.5,10))
-  par(tcl=0)
-  image(1:30,1,as.matrix(as.numeric(anno.smpls$BOR)),col=brewer.pal(3,"Set1")[c(1,3,2)],axes=F,
-       xlab="",ylab="",xlim=c(-0.5,31.5))
-  axis(2,at=1,labels=c("BOR"),las=1)
-  ##
-  par(mar=c(0,7,.5,10))
-  image(1:30,1,as.matrix(as.numeric(anno.smpls$TimePoint)),col=brewer.pal(4,"Set2"),axes=F,
-       xlab="",ylab="",xlim=c(-0.5,31.5))
-  axis(2,at=1,labels=c("Time point"),las=1)
-  ##
-  par(mar=c(0,7,.5,10))
-  image(1:30,1,as.matrix(as.numeric(anno.smpls$Subtype)),col=brewer.pal(3,"Dark2"),axes=F,
-       xlab="",ylab="",xlim=c(-0.5,31.5))
-  axis(2,at=1,labels=c("Subtype"),las=1)
-
-  ##
-  par(mar=c(0,7,.5,10))
-  image(1:30,1,as.matrix(as.numeric(factor(anno.smpls$gBRCA.status))),col=c("black","grey80"),axes=F,
-       xlab="",ylab="",xlim=c(-0.5,31.5))
-  axis(2,at=1,labels=c("gBRCA.status"),las=1)
-
-  ##
-  par(mar=c(0,7,.5,10))
-  par(tcl=0)
-  image(1:30,1,as.matrix(as.numeric(anno.smpls$Patient)),col=brewer.pal(11,"Set3"),axes=F,
-       xlab="",ylab="",xlim=c(-0.5,31.5))
-  axis(2,at=1,labels=c("Patient"),las=1)
 }
